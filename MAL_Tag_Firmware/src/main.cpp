c23f99e79221bd29fa193abb9d388dc29e160d25
shroomcloud <opschon@gmail.com>
2021-01-05 22:06:47 +0100

    bruh

--- MAL_Tag_Firmware/src/main.cpp
+++ MAL_Tag_Firmware/src/main.cpp
@@ -1,12 +1,38 @@
-#include <Arduino.h>
 #include <BLEDevice.h>
-#include <BLEUtils.h>
 #include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+#include <Arduino.h>
 
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+#define dacPin1 25
 
-class MyCallbacks: public BLECharacteristicCallbacks {
+bool deviceConnected = false;
+uint8_t anchorPing = 0;
+/* Define the UUID for our Custom Service */
+
+
+BLECharacteristic dacCharacteristic(
+  BLEUUID(CHARACTERISTIC_UUID), 
+  BLECharacteristic::PROPERTY_READ | 
+  BLECharacteristic::PROPERTY_WRITE
+);
+
+//Server Callbacks handling
+
+class ServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+    }
+};
+
+class charCallbacks: 
+  public BLECharacteristicCallbacks {
     void onWrite(BLECharacteristic *pCharacteristic) {
       std::string value = pCharacteristic->getValue();
       if (value.length() > 0) {
@@ -18,27 +44,44 @@ class MyCallbacks: public BLECharacteristicCallbacks {
     }
 };
 
+
 void setup() {
   Serial.begin(9600);
+  pinMode(dacPin1, OUTPUT);
 
-  BLEDevice::init("ESP32 BLE example");
+
+  BLEDevice::init("Tag_ESP32");
   BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new ServerCallbacks());
+  
   BLEService *pService = pServer->createService(SERVICE_UUID);
-  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
-                                         CHARACTERISTIC_UUID,
-                                         BLECharacteristic::PROPERTY_READ |
-                                         BLECharacteristic::PROPERTY_WRITE
-                                       );
 
-  pCharacteristic->setCallbacks(new MyCallbacks());
-
-  pCharacteristic->setValue("Hello World");
+  //I still don't know what the callback is exactly
+  dacCharacteristic.setCallbacks(new charCallbacks());
+  dacCharacteristic.setValue("0");
+  pService->addCharacteristic(&dacCharacteristic);
+  pServer->getAdvertising()->addServiceUUID(SERVICE_UUID);
   pService->start();
-
-  BLEAdvertising *pAdvertising = pServer->getAdvertising();
-  pAdvertising->start();
+  pServer->getAdvertising()->start();
+  Serial.println("Waiting for a Client to connect...");
 }
 
 void loop() {
-  delay(2000);
+  anchorPing = *(dacCharacteristic.getData());
+  if(deviceConnected == true) {
+    Serial.println(anchorPing);
+    // BLECharacteristics setValue() i haven't figured it out how to set to 0, but setting value to "0" gives me the value 48 (it's ascii)
+    if(anchorPing != 48 ) {
+
+      dacWrite(dacPin1, 255);
+
+    } else {
+
+      dacWrite(dacPin1, 0);
+
+    }
+
+
+  } else dacWrite(dacPin1,0);
+delay(50);
 }
